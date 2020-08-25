1119ebe5f9b8bd670fbc601154bcedf4dac03af5
KatherineReyes10 <70220611+KatherineReyes10@users.noreply.github.com>
2020-08-25 13:06:15 -0400

    Add files via upload

---
+++ EmitidorDeSonido.py
@@ -0,0 +1,10 @@
+from playsound import playsound
+class EmitidorDeSonido:
+    def emitirSonidoCorto(self):
+        playsound("short.mp3")
+
+    def emitirSonidoLargo(self):
+        playsound("long.mp3")
+
+
+
---
+++ ObtenedorDeEntrada.py
@@ -0,0 +1,3 @@
+class ObtenedorDeEntrada:
+    def getEntrada(self):
+        return input("Introduce el texto que desea convertir a morse:\n")
